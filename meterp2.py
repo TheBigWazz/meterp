29c4f12f4cacc4287cb3979319dcfa6d5c625294
TheBigWazz <80355486+TheBigWazz@users.noreply.github.com>
2023-01-09 12:08:32 -0800

    Add files via upload

--- meterp2.py
+++ meterp2.py
@@ -1,131 +1,144 @@
 import pyaudio
-import struct
-import math
-import tkinter as tk
-from functools import partial  # Import partial from functools
+import audioop
+from PyQt5 import QtWidgets, QtGui, QtCore
 
-# Set up tkinter
-root = tk.Tk()
-root.title("Audio Level Meter")
-root.configure(background="#181818")  # Change background color to YouTube dark mode color
+# Create the main window
+app = QtWidgets.QApplication([])
+window = QtWidgets.QWidget()
+window.setWindowTitle("Decibel Meter")
 
-# Set up pyaudio
+# Set the background color and text color for the widget
+window.setStyleSheet("""
+    QWidget {
+        background-color: #2D2D2D;
+        color: white;
+    }
+""")
+
+# Create a label
+label = QtWidgets.QLabel(text="")
+
+# Set the text color for the label
+label.setStyleSheet("color: white;")
+
+# Create a progress bar
+progress_bar = QtWidgets.QProgressBar()
+
+# Set the style for the progress bar
+progress_bar.setStyleSheet("""
+    QProgressBar {
+        background-color: #2D2D2D;
+        color: white;
+        border: 1px solid white;
+        border-radius: 5px;
+        text-align: center;
+    }
+    QProgressBar::chunk {
+        background-color: white;
+        width: 10px;
+        margin: 1px;
+    }
+""")
+
+
+# Get the list of available devices
 p = pyaudio.PyAudio()
 
-# Set up the input stream
-CHUNK = 1024
-WIDTH = 2
-CHANNELS = 1
-RATE = 44100
+devices = {}
 
-# Define the global stream variable
-global stream
-stream = None
+# Get the device information
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        name = p.get_device_info_by_host_api_device_index(0, i).get('name')
+        devices[name] = i
 
-# Set up the visualizer
-BAR_WIDTH = 20
-BAR_HEIGHT = 400
+p.terminate()
 
-# Create the bar graph widget
-bar = tk.Canvas(root, width=BAR_WIDTH + 50, height=400, highlightthickness=0)  # Remove highlightthickness to remove white border
-bar.config(bg='#181818')  # Change background color to YouTube dark mode color
-bar.pack()
+# Create a dropdown menu
+device_var = QtWidgets.QComboBox()
+device_var.addItem("Select Device")  # Add "Select Device" as the default option
+device_var.addItems(list(devices.keys()))
+
+# Choose the device and start the stream
+device_index = -1  # Set the default device index to -1
+if device_var.currentText() != "Select Device":
+    device_index = devices[device_var.currentText()]
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
+                input_device_index=device_index, frames_per_buffer=1024)
 
 
 
-# Function to open the selected input device
-def open_device():
+# Define a function to update the label and progress bar
+def update_display():
+    global device_index
+    
+    # Calculate the RMS and update the label
+    data = stream.read(1024)
+    rms = audioop.rms(data, 2)
+    label.setText(str(rms))
+    
+    # Update the progress bar
+    progress_bar.setValue(rms)
+    progress_bar.repaint()
+    QtCore.QTimer.singleShot(100, update_display)  # Call this function again in 100ms
+
+
+
+# Start updating the display
+update_display()
+
+
+
+# Define a function to change the device
+def change_device(*args):
     global stream
-    global max_db
-    device_index = None
-    device_name = device_var.get()
-    if device_name == "None":
-        return
-    device_index = None
-    for i in range(p.get_device_count()):
-        device_info = p.get_device_info_by_index(i)
-        if device_info["name"] == device_name:
-            device_index = device_info["index"]
-            break
-    if device_index is None:
-        return
-    if stream is not None:
-        stream.stop_stream()
-        stream.close()
-    stream = p.open(
-        format=p.get_format_from_width(WIDTH),
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        output=False,
-        input_device_index=device_index,
-        frames_per_buffer=CHUNK,
-    )
-        # Get the maximum dB level of the input device
-    max_db =  80
-        
-    # Update the bar graph
-    if max_db:  # Check if max_db is defined
-        update_bar()
-    
-    
-    
-# Function to update the bar graph
-def update_bar():
-    # Read data from the input stream
-    data = stream.read(CHUNK)
-    data = struct.unpack(str(CHUNK) + 'h', data)
+    global p
+    global progress_bar
 
-    # Calculate the RMS level of the audio
-    rms = math.sqrt(sum(x ** 2 for x in data) / len(data))
-    # Calculate the SPL in dB relative to a reference value of 0 dB
-    
-    reference = 20  # Reference value for SPL in pascals
-    db = math.log10(rms / reference) - 0  # Subtract 0 dB to make the reference value 0 dB
-    
-    if db > max_db:
-        db = max_db
+    # Stop and close the current stream
+    stream.stop_stream()
+    stream.close()
 
-    # Calculate the bar level on a non-logarithmic scale
-    bar_level = int((db / 80) * BAR_HEIGHT)
-    if bar_level > BAR_HEIGHT - 20:  # Leave room for the dB markings
-        bar_level = BAR_HEIGHT - 20
+    # Get the selected device index
+    if device_var.currentText() == "Select Device":
+        device_index = -1  # Set the device index to -1 if "Select Device" is selected
+    else:
+        device_index = devices[device_var.currentText()]
 
-    # Update the bar graph
-    bar.delete("all")
-    bar.create_rectangle(0, BAR_HEIGHT - bar_level, BAR_WIDTH, BAR_HEIGHT, fill="#00ff00")
-    bar.create_rectangle(0, 0, BAR_WIDTH, BAR_HEIGHT - bar_level, fill="#181818")
+    # Set the maximum value of the progress bar based on the maximum input channel count
+    max_input_channels = p.get_device_info_by_host_api_device_index(0, device_index).get('maxInputChannels')
+    if max_input_channels == 1:
+        progress_bar.setMaximum(32768)
+    else:
+        progress_bar.setMaximum(32768 * max_input_channels)
 
-    # Add dB markings
-    for i in range(0, int(max_db) + 1, 10):
-        y = (i / 80) * BAR_HEIGHT  # Calculate y position based on the dB value
-        bar.create_line(BAR_WIDTH - 10, y, BAR_WIDTH + 1, y, fill="#fff")
-        bar.create_text(BAR_WIDTH + 10, y, text=str(i) + " dB", anchor="w", fill="#fff")
-    
-    # Schedule the next update
-    root.after(50, update_bar)
+    # Create a new stream with the selected device
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
+                    input_device_index=device_index, frames_per_buffer=1024)
 
 
 
-# Get a list of available input devices
-device_options = ["None"]
-for i in range(p.get_device_count()):
-    device_info = p.get_device_info_by_index(i)
-    device_options.append(device_info["name"])
 
-# Set the default input device
-device_var = tk.StringVar(root)
-device_var.set(device_options[0])
+# Bind the function to the "currentIndexChanged" event
+device_var.currentIndexChanged.connect(change_device)
 
-# Create the device selection menu
-device_menu = tk.OptionMenu(root, device_var, *device_options, command=partial(open_device))  # Use partial to create new function that calls open_device with no arguments
-device_menu.config(bg="#181818", fg="#fff", activebackground="#303030", activeforeground="#fff", highlightthickness=0)  # Change colors to YouTube dark mode colors
-device_menu.pack()
+# Create a layout and add the widgets
+layout = QtWidgets.QVBoxLayout()
+layout.addWidget(device_var)
+layout.addWidget(label)
+layout.addWidget(progress_bar)
+window.setLayout(layout)
 
-# Create the open device button
-open_button = tk.Button(root, text="Open Device", command=partial(open_device))  # Use partial to create new function that calls open_device with no arguments
-open_button.config(bg="#181818", fg="#fff", activebackground="#303030", activeforeground="#fff", highlightthickness=0)  # Change colors to YouTube dark mode colors
-open_button.pack()
+# Show the window
+window.show()
 
-# Start the tkinter event loop
-root.mainloop()
+# Run the PyQt event loop
+app.exec_()
+
+# Clean up
+stream.stop_stream()
+stream.close()
+p.terminate()
