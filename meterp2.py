bcbb76edb9205c179850a19662748d41bec54a66
TheBigWazz <80355486+TheBigWazz@users.noreply.github.com>
2023-01-11 10:25:51 -0800

    Add files via upload

--- meterp2.py
+++ meterp2.py
@@ -1,11 +1,16 @@
 import pyaudio
 import audioop
 from PyQt5 import QtWidgets, QtGui, QtCore
+from math import log10
+
+# Import the QPushButton class
+from PyQt5.QtWidgets import QPushButton
+import sounddevice as sd
 
 # Create the main window
 app = QtWidgets.QApplication([])
 window = QtWidgets.QWidget()
-window.setWindowTitle("Decibel Meter")
+window.setWindowTitle("Meterp")
 
 # Set the background color and text color for the widget
 window.setStyleSheet("""
@@ -29,16 +34,17 @@ progress_bar.setStyleSheet("""
     QProgressBar {
         background-color: #2D2D2D;
         color: white;
-        border: 1px solid white;
+        border: 1px solid black;
         border-radius: 5px;
         text-align: center;
     }
     QProgressBar::chunk {
-        background-color: white;
-        width: 10px;
+        background-color: green;
+        width: 1px;
         margin: 1px;
     }
 """)
+
 
 
 # Get the list of available devices
@@ -66,25 +72,29 @@ device_index = -1  # Set the default device index to -1
 if device_var.currentText() != "Select Device":
     device_index = devices[device_var.currentText()]
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
-                input_device_index=device_index, frames_per_buffer=1024)
 
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, input_device_index=device_index, frames_per_buffer=1024)
 
+  
 
-# Define a function to update the label and progress bar
+ref = 1
 def update_display():
-    global device_index
-    
-    # Calculate the RMS and update the label
-    data = stream.read(1024)
-    rms = audioop.rms(data, 2)
-    label.setText(str(rms))
-    
-    # Update the progress bar
-    progress_bar.setValue(rms)
+    global device_index, muted
+    if not stream.is_active:
+        label.setText('MUTED')
+        
+    else:
+        data = stream.read(1024)
+        rms = audioop.rms(data, 2)
+        try:
+            dB = round(20 * log10(rms / ref))
+            label.setText(str(dB) + " dB")
+            progress_bar.setValue(rms)
+        except ValueError:
+            label.setText('Muted')
+            
     progress_bar.repaint()
-    QtCore.QTimer.singleShot(100, update_display)  # Call this function again in 100ms
-
+    QtCore.QTimer.singleShot(1, update_display)
 
 
 # Start updating the display
@@ -92,21 +102,19 @@ update_display()
 
 
 
-# Define a function to change the device
 def change_device(*args):
     global stream
     global p
     global progress_bar
-
-    # Stop and close the current stream
-    stream.stop_stream()
-    stream.close()
 
     # Get the selected device index
     if device_var.currentText() == "Select Device":
         device_index = -1  # Set the device index to -1 if "Select Device" is selected
     else:
         device_index = devices[device_var.currentText()]
+    
+    if device_index == -1:
+        return  # Exit the function if "Select Device" is selected
 
     # Set the maximum value of the progress bar based on the maximum input channel count
     max_input_channels = p.get_device_info_by_host_api_device_index(0, device_index).get('maxInputChannels')
@@ -115,27 +123,36 @@ def change_device(*args):
     else:
         progress_bar.setMaximum(32768 * max_input_channels)
 
-    # Create a new stream with the selected device
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
-                    input_device_index=device_index, frames_per_buffer=1024)
+    # Stop and close the current stream
+    stream.stop_stream()
+    stream.close()
 
+    # Restart the stream with the new device
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, input_device_index=device_index, frames_per_buffer=1024)
 
+    # Start updating the display
+    update_display()
 
-
-# Bind the function to the "currentIndexChanged" event
+   
+# Connect the device dropdown menu's currentIndexChanged signal to the change_device function
 device_var.currentIndexChanged.connect(change_device)
 
-# Create a layout and add the widgets
+# Create a layout for the widgets
 layout = QtWidgets.QVBoxLayout()
+
+# Add the widgets to the layout
 layout.addWidget(device_var)
 layout.addWidget(label)
 layout.addWidget(progress_bar)
+
+
+# Set the layout for the main window
 window.setLayout(layout)
 
-# Show the window
+# Show the main window
 window.show()
 
-# Run the PyQt event loop
+# Run the application loop
 app.exec_()
 
 # Clean up
